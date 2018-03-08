5c7ea3f340bd66096005b5f0a09623c0b9b25c20
loire <loire@elitebook.localdomain>
2018-03-08 07:45:41 +0100

    Fire with step 3

--- Fire_ClassI.R
+++ Fire_ClassI.R
@@ -5,16 +5,12 @@ silent <- list.files(pattern="csv|fasta")  %>% file.remove()
 # CLASS I ----------------------------
 PATH_TO_CSV_FOLDER = "data/classI"
 cooked <- PATH_TO_CSV_FOLDER %>% cook_I() %T>% print
-# save(cooked, file="cooked_temp.rda")
-#load("cooked_temp.rda")
-# beepr::beep()
 
 # then 
 PATH_TO_BLAST_TXT  = "blast_ClassI.txt"
 digested <- cooked %>% digest_I_and_II(PATH_TO_BLAST_TXT) %T>%
   export_csv("3_blast_mismatches_I") %>% print
-# save(digested, file="digested_temp.rda")
-# load("digested_temp.rda")
-# beepr::beep()
+
+source("FilterAndRankPeptides_ClassI.R")
 
 # STEP3
