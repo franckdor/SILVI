0d5177021c6681da1e745a4c9d55880de8c50ff7
Franck Dorkeld <franck.dorkeld@inrae.fr>
2020-11-06 10:16:52 +0100

    add first code to prepare  tests

--- tests/testthat/test_SILVI.R
+++ tests/testthat/test_SILVI.R
@@ -6,6 +6,50 @@ library(testthat)
 # source("../../code/SILVI.R") # This is only needed if your project is not a package
 # source("/Users/dorkeld/Documents/old_documents/travail_mac/projets/Code_LOIRE_SILVI/14_oct_test_fin/SILVI/code/SILVI.R")
 
+
+
+# PREREQUIS POUR LES TESTS
+# lecture d'un fichier fasta de test
+# pour faire une dataframe de test
+# puis lancement de add_best_From_blast pour avoir un dataframe correcte
+
+FASTA_FILE="../tests_data/1_blastme_I_pf_csp.fasta"
+
+peptide <- c()         #vector() # un vecteur de peptides
+
+con = file(FASTA_FILE, "r")
+while ( TRUE ) {
+  line = readLines(con, n = 1)
+  if ( length(line) == 0 ) {
+    break
+  }
+  #print(line)
+  if (! startsWith(line, ">")  ) {
+    # print (line)
+    peptide <- c(peptide,line)
+  }
+}
+
+close(con)
+
+peptide_df <- data.frame(peptide, stringsAsFactors=FALSE)
+# df$peptide <- factor(df$peptide)
+peptide_df <- as_tibble(peptide_df)
+
+cat(" len peptide : ",length(peptide_df$peptide),"\n")
+expect_equal(length(peptide_df$peptide), 228)
+
+# lecture  d'un fichier d'alignement après blast
+PATH_TO_BLAST_TXT="../tests_data/pf_csp_alignment_I.txt"
+add_best_df <- add_best_from_blast(peptide_df,PATH_TO_BLAST_TXT)
+print(add_best_df)
+
+
+
+
+
+
+
 #import_all_protein_predictor_class1
 
 #  PRODUIT  PAR count_mismatches ou count_mismatches_fd appelé par à mettre dans test_SILVI.R
@@ -18,7 +62,7 @@ library(testthat)
 # TESTER cook_II
 
 
-digest_I_and_II
+# digest_I_and_II
 
 
 # test : digest_I_and_II
